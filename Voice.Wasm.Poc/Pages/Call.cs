e73e29c72eceabe9d81f3083489da911bc2af1cf
Venu Lekkala <venu.lekkala@ctg.com>
2023-05-15 21:34:58 +0530

    Add project files.

---
+++ Voice.Wasm.Poc/Pages/Call.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.JSInterop;
+namespace Voice.Wasm.Poc.Pages;
+
+public class JsInteropClasses : IDisposable
+{
+    private readonly IJSRuntime js;
+
+    public JsInteropClasses(IJSRuntime js)
+    {
+        this.js = js;
+    }
+
+    public async ValueTask TickerChanged(string serverName, string userName, string password)
+    {
+        await js.InvokeVoidAsync("displayTickerAlert1", serverName, userName, password);
+    }
+
+    public void Dispose()
+    {
+    }
+}
---
+++ Voice.Wasm.Poc/wwwroot/js/poc.js
@@ -0,0 +1,35 @@
+﻿function Register(ServerText, UserName, Password) {
+    if (webphone_api.webphone_loaded == true) {
+        console.log("Test2");
+        /* if (state === 'loaded') {*/
+        webphone_api.setparameter('serveraddress', ServerText);
+        webphone_api.setparameter('username', UserName);
+        webphone_api.setparameter('password', Password);
+        webphone_api.start();
+        setTimeout(3000);
+        var res = webphone_api.isregistered();
+        return res;
+    }
+}
+
+function pageLoad() {
+    webphone_api.onCallStateChange(function (status, direction, peer, peername) {
+        if (direction == 2 && status == "ringing") {
+            alert(status);
+            //alert("Please answer Incoming Call");
+        }
+    });
+}
+
+function makeACall(NUMBER) {
+    webphone_api.call(NUMBER);
+    pageLoad();
+}
+
+function acceptCall() {
+    webphone_api.accept();
+}
+function disconnectCall() {
+    webphone_api.hangup();
+}
+
